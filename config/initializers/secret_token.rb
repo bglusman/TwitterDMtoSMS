dd74083fb0a148fbb5a1d29e96437d1213ee7d51
bglusman <brian@enjyn.com>
2010-11-17 15:30:22 -0500

    Revert "Initial Commit"
    This reverts commit 0bbb50e35066413b44f158f2ea6854096cf7ece9.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-TwitterDmToSms::Application.config.secret_token = '5b79858bb728539654f714a92a482bb64ba255f7db9e390eb773f88d73e0aa08bf3187d8aac0c11ba7bc3217d3b4af62a854068eb49efbc38a3d91639bcb09a8'
